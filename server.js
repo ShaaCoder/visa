0adce05a8144b662cebf3d3a02ccbcd2291ba520
ShaaCoder <125294108+ShaaCoder@users.noreply.github.com>
2024-12-12 14:52:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,8 +16,12 @@ const MONGO_URI = process.env.MONGO_URI;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173', // Your frontend origin here
+  origin: 'http://localhost:5173', // Your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow the methods you need
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow headers
+  credentials: true, // If you need credentials (cookies or headers)
 }));
+
 app.use(bodyParser.json());
 
 // Connect to MongoDB
